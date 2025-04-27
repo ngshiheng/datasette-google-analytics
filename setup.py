d917c9231cf3ae54b9e71eef4ea80d272352092c
Jerry Ng <ngshiheng@gmail.com>
2025-04-27 13:33:48 +0800

    feat: init

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+VERSION = "0.1.0"
+
+setup(
+    name="datasette-google-analytics",
+    description="Datasette plugin that adds Google Analytics tracking",
+    author="Jerry Ng",
+    url="https://github.com/ngshiheng/datasette-google-analytics",
+    license="MIT",
+    version=VERSION,
+    py_modules=["datasette_google_analytics"],
+    entry_points={"datasette": ["google_analytics = datasette_google_analytics"]},
+    install_requires=["datasette"],
+)
