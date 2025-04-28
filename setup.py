b9719e2a5c3145f477015070ebfbe6c90766e266
Jerry Ng <ngshiheng@gmail.com>
2025-04-28 19:05:08 +0800

    chore: fix build requirements in setup.py by adding missing comma for wheel

--- setup.py
+++ setup.py
@@ -42,8 +42,8 @@ setup(
             "python-semantic-release",
         ],
         "build": [
+            "wheel",
             "build",
-            "wheel"
             "setuptools",
         ],
     },
